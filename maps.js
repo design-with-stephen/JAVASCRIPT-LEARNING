18281903ba168515bf3b6255c76860e957579dd3
design-with-stephen <stephen211.me@gmail.com>
2025-04-19 00:03:59 +0300

    learnt iteration in of maps

--- maps.js
+++ maps.js
@@ -21,4 +21,113 @@ console.log(fruits);
 
 // accessing elements in a map (-> use the get() method)
 const result = fruits.get("banana")
-console.log(result); // undefined
+console.log(result); //
+
+// to return the size of a map use the map.size property, it will return the number of elements in the map
+
+// example
+
+fruits.size;
+console.log(fruits.size);  // it will return 4
+
+// to delete an element in the map use the delete() function
+
+fruits.delete("apple");
+
+console.log(fruits)
+
+
+// to remove all the elements from a map the, clear() method can be use, this will clear all the elements in a map
+
+fruits.clear();
+
+console.log(fruits)
+
+// to check the existence of element in a map, the has() method is used
+
+const exist = fruits.has("apple")
+
+if (exist) {
+    console.log("item exist")
+} else {
+    console.log("item does not exist")
+}
+
+// to loop through maps , the forEach() loop can be used for example
+
+
+
+const allFruits = new Map([
+    ["apple", 5],
+    ["banana", 10],
+    ["cherry", 15],
+])
+
+// let i = "";
+// allFruits.forEach((value, key) => {
+//     i += ` ${key} : ${value}`;
+//     console.log(i);
+//     // i += key + '=' + value;
+//     // console.log(i);
+// })
+
+allFruits.entries();
+
+// summery
+
+// Map
+// Map is a collection of keyed data items, just like an Object. But the main difference is that
+// Map allows keys of any type.
+
+// Methods and properties are:
+
+// new Map() – creates the map.
+// map.set(key, value) – stores the value by the key.
+// map.get(key) – returns the value by the key, undefined if key doesn’t exist in map.
+// map.has(key) – returns true if the key exists, false otherwise.
+// map.delete(key) – removes the element (the key/value pair) by the key.
+// map.clear() – removes everything from the map.
+// map.size – returns the current element count.
+
+
+
+
+
+
+// Iteration over Map
+// For looping over a map, there are 3 methods:
+
+// map.keys() – returns an iterable for keys,
+// map.values() – returns an iterable for values,
+// map.entries() – returns an iterable for entries [key, value], it’s used by default in for..of.
+
+
+
+const animals = new Map([
+    ["dog", 5],
+    ["cat", 4],
+    ["cow", 3],
+    ["goat",2]
+])
+
+//iterating over the keys
+for (let domestic of animals.keys()) {
+    console.log(domestic)
+}
+
+// iterating over values or amount
+
+for (let amount of animals.values()) {
+    console.log(amount)
+}
+
+// iterating over entries, it iterates over the keys and value in a map
+
+
+for (let entry of animals.entries()) {
+    console.log(entry);
+}
+
+// NB
+//  the insertion order of the elements in the map is preserved, unlike in objects hence
+//  the order of the elements in iteration result
